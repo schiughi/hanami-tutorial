8ffb9448305e88b45acdc1ff88513ae8e620c3c8
schiughi <schiughi@gmail.com>
2017-12-29 16:37:20 +0900

    create page book#index

--- spec/web/views/books/index_spec.rb
+++ spec/web/views/books/index_spec.rb
@@ -1,15 +1,34 @@
 require_relative '../../../spec_helper'
 
 describe Web::Views::Books::Index do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[books: []] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/books/index.html.erb') }
   let(:view)      { Web::Views::Books::Index.new(template, exposures) }
   let(:rendered)  { view.render }
 
-  it 'exposes #foo' do
-    skip 'This is an auto-generated test. Edit it and add your own tests.'
+  it 'exposes #books' do
+    view.books.must_equal exposures.fetch(:books)
+  end
 
-    # Example
-    view.foo.must_equal exposures.fetch(:foo)
+  describe 'when there are no books' do
+    it 'show a placeholder message' do
+      rendered.must_include('There are no books yet')
+    end
+  end
+
+  describe 'when there are books' do
+    let(:book1)  { Book.new(title: 'Test Driven Development' , isbn: '978-0321146533') }
+    let(:book2)  { Book.new(title: 'Domain-Driven Design', isbn: '978-0321125217') }
+    let(:exposures) { Hash[books: [book1, book2]] }
+
+    it 'lists them all' do
+      rendered.scan(/class="book"/).count.must_equal 2
+      rendered.must_include('Test Driven Development')
+      rendered.must_include('Domain-Driven Design')
+    end
+
+    it 'hides the placeholder message' do
+      rendered.wont_include('There are no books yet.')
+    end
   end
 end
